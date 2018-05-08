59c76d86284db7dce27f3f8ff5a40eb159cfb673
Michał Rotko <michalr835@gmail.com>
2018-05-08 12:23:50 +0200

    fix roles test

--- src/test/java/pl/shopgen/models/RoleRepositoryTest.java
+++ src/test/java/pl/shopgen/models/RoleRepositoryTest.java
@@ -1,9 +1,15 @@
 package pl.shopgen.models;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RoleRepositoryTest extends SimpleMongoRepositoryTest<Role, RoleRepository> {
 
     @Override
@@ -30,6 +36,4 @@ public class RoleRepositoryTest extends SimpleMongoRepositoryTest<Role, RoleRepo
         newRole.setName(newRole.getName() + "nowa");
         return newRole;
     }
-
-
 }
